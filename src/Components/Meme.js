39bece5e84b9ae0b3236b5973665cc0b5f482011
andronachi09 <tudor.andronachi09@gmail.com>
2023-04-02 23:06:51 +0200

    added text to the image

--- src/Components/Meme.js
+++ src/Components/Meme.js
@@ -24,21 +24,42 @@ export default function Meme(){
         })
     }
 
+    function handleChange(event) {
+        const {name, value, type, checked} = event.target
+        setMemeImage(prevState => {
+            return {
+                ...prevState,
+                [name]: type === "checkbox" ? checked : value
+            }
+        })
+    }
+
     return(
         <main>
             <div className="meme--form">
                 <input 
                     type="text"
                     placeholder="Top text"
-                    className="form--input"/>
+                    className="form--input"
+                    name="topText"
+                    value={memeImage.topText}
+                    onChange={handleChange}
+                />
                 <input 
                     type="text"
                     placeholder="Bottom text"
                     className="form--input"
-                    />
+                    name="bottomText"
+                    value={memeImage.bottomText}
+                    onChange={handleChange}
+                />
                 <button className="form--button" onClick={getMemeImage}>Get a new meme image 🖼️</button>
             </div>
-            <img src={memeImage.randomImage} className="meme--image"></img>
+            <div className="meme">
+                <img src={memeImage.randomImage} className="meme--image"></img>
+                <h2 className="meme--text top">{memeImage.topText}</h2>
+                <h2 className="meme--text bottom">{memeImage.bottomText}</h2>
+            </div>     
         </main>
     )
 }
