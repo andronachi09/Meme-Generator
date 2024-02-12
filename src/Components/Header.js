c17b8375dbf05c946a4cbb7aa75431a415dfe055
Tudor Andronachi <65310331+andronachi09@users.noreply.github.com>
2024-02-12 11:09:07 +0100

    chore: gitignore and yarn.lock

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -7,7 +7,6 @@ export default function Header() {
         <header className="header">
             <img className="header--image" src={Trollface} alt="Logo"></img>
             <h2 className="header--title">Meme Generator</h2>
-            <h4 className="header--h4">React Course - Project 3</h4>
         </header>
     )
 }
